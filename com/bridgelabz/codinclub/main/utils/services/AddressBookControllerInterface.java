16520043e55bb304032f86fec50a096fc68c957e
aravind <aravindersuram@gmail.com>
2020-05-05 02:37:09 +0530

    phonebook

---
+++ com/bridgelabz/codinclub/main/utils/services/AddressBookControllerInterface.java
@@ -0,0 +1,5 @@
+package com.codeinclub.bridgelabz.services;
+
+public interface AddressBookControllerInterface {
+    public String createNewAddressBook(String fileName);
+}
---
+++ com/bridgelabz/codinclub/main/utils/services/AddressBookInterface.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.codeinclub.services;
+import com.bridgelabz.codeinclub.model.Person;
+import java.io.IOException;
+
+public interface AddressBookInterface {
+
+    public String addPersonRecord(Person person, String file_path);
+    public String deleteRecord(String... strings) throws IOException;
+    public String  sortListByName(String file_path) throws IOException;
+    public String sortListByZip(String file_path) throws IOException;
+    public void printEntries(String file_path) throws IOException;
+    public String updatePersonData(String... strings) throws IOException;
+}
