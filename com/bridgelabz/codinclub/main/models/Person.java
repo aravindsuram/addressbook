ea8fc1cad20d4cbb8595e3eb7e5120209b6c0054
aravind <aravindersuram@gmail.com>
2020-05-05 02:38:30 +0530

    phonebook

---
+++ com/bridgelabz/codinclub/main/models/Person.java
@@ -0,0 +1,72 @@
+package com.bridgelabz.model;
+/** Person class
+@ author: Aravind
+*/
+
+public class Person {
+
+    // Declare variable
+    private String firstName;
+    private String lastName;
+    private String city;
+    private String state;
+    private String zip;
+    private String phoneNumber;
+
+    // Default constructor
+    public Person() {
+    }
+
+    // Parameterized constructor
+    public Person(String firstName, String lastName, String city, String state, String zip, String phoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+    }
+
+    // Getter methods
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    // Setter methods
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
